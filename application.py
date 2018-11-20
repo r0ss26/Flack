e011511754f1d3bf2e69fefee1b40e99b2e3a7cc
r0ss26 <rossbaker32@gmail.com>
2018-11-20 18:27:20 +1100

    added html layout template

--- application.py
+++ application.py
@@ -1,13 +1,32 @@
 import os
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request, session, redirect, url_for
 from flask_socketio import SocketIO, emit
+from flask_sqlalchemy import SQLAlchemy
 
+# Initialise and configure flask application
 app = Flask(__name__)
-app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+app.config["SECRET_KEY"] = os.environ.get('SECRET_KEY')
+
+# Configure database connection
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
+db = SQLAlchemy(app)
+
+# Initialise SocketIO
 socketio = SocketIO(app)
 
-
-@app.route("/")
+@app.route("/", methods=["GET"])
 def index():
-    return render_template("index.html")
+    return render_template("login.html")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    if request.method == 'GET':
+        return render_template("login.html")
+    else:
+        return
+
+@app.route("/home", methods=["GET", "POST"])
+def home():
+    if request.method == 'GET':
+        return render_template("main.html")
