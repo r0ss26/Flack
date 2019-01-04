187ceb262f05625ed842ab4e6e3092a9b274cbad
r0ss26 <rossbaker32@gmail.com>
2019-01-04 14:59:19 +1100

    socketIO config

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -7,12 +7,12 @@ $(document).ready(function () {
   // Connect to websocket
   var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port)
 
-  // Let the server know which room the user is in
-  socket.emit('join', { 'username': $('#username').html(), 'room': room })
-
   /* Once the client has connected to the websocket, add functionality for the buttons
   to change channels and post message */
   socket.on('connect', function () {
+    // Let the server know which room the user is in
+    socket.emit('join', { 'username': $('#username').html(), 'room': room })
+
     // Add event listeners to the channel buttons to allow the user to change channels
     document.querySelectorAll('.channel').forEach(channel => {
       channel.onclick = () => {
